f770fb2fccd15d2d5c79076df200f696ce6b3b21
Sarvin Nami <srvn0nm@gmail.com>
2023-06-27 15:59:08 +0330

    a function to change the frequency of a signal

--- DigitalRadio.py
+++ DigitalRadio.py
@@ -19,3 +19,11 @@ def apply_bandpass_filter(signal_data, cutoff_freq):
     b, a = signal.butter(4, normalized_cutoff_freq, btype='lowpass')
     filtered_signal = signal.lfilter(b, a, signal_data)
     return filtered_signal
+
+
+# changing the frequency of the signal
+def change_frequency(signal_data, freq_shift):
+    time = np.arrange(len(signal_data)) / SAMPLING_RATE
+    complex_signal = signal_data * np.exp(1j * 2 * np.pi * freq_shift * time)
+    shifted_signal = np.real(complex_signal)
+    return shifted_signal
