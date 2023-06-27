4f741611f3118b62db47830b9290d71fc77f7f1d
Sarvin Nami <srvn0nm@gmail.com>
2023-06-27 15:55:38 +0330

    a function to filter the signal with the given frequency

--- DigitalRadio.py
+++ DigitalRadio.py
@@ -5,12 +5,17 @@ import sounddevice as sd
 SAMPLING_RATE = 480000
 DURATION = 10
 
+
 # reading inputs from the file
 def read_input_file(file_path):
     with open(file_path, 'r') as file:
         signal_data = [float(line.strip()) for line in file]
     return signal_data
 
-# filtering the signal with the given frequency
-def app
 
+# filtering the signal with the given frequency
+def apply_bandpass_filter(signal_data, cutoff_freq):
+    normalized_cutoff_freq = cutoff_freq / (SAMPLING_RATE / 2)
+    b, a = signal.butter(4, normalized_cutoff_freq, btype='lowpass')
+    filtered_signal = signal.lfilter(b, a, signal_data)
+    return filtered_signal
