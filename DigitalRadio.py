a379c5cdde3afa127f8b0ed5f541a3e2021ea9d9
sa9978 <sara.rhn78@gmail.com>
2023-06-29 22:43:31 +0330

    add main program loop

--- DigitalRadio.py
+++ DigitalRadio.py
@@ -2,36 +2,91 @@ import numpy as np
 import scipy.signal as signal
 import sounddevice as sd
 
-SAMPLING_RATE = 480000
+SAMPLING_RATE = 48000
 DURATION = 10
 
 
-# reading inputs from the file
 def read_input_file(file_path):
+    """Reads the input signal from a file.
+
+    Args:
+        file_path (str): The path to the input file.
+
+    Returns:
+        list: The signal data read from the file.
+    """
     with open(file_path, 'r') as file:
         signal_data = [float(line.strip()) for line in file]
     return signal_data
 
 
-# filtering the signal with the given frequency
 def apply_bandpass_filter(signal_data, cutoff_freq):
+    """Applies a bandpass filter to the signal.
+
+    A bandpass filter allows a certain range of frequencies to pass through while attenuating frequencies outside
+    that range. In this function, a Butterworth lowpass filter is used to achieve the bandpass effect.
+
+    Args:
+        signal_data (list): The input signal.
+        cutoff_freq (float): The cutoff frequency of the filter.
+
+    Returns:
+        numpy.ndarray: The filtered signal.
+    """
     normalized_cutoff_freq = cutoff_freq / (SAMPLING_RATE / 2)
     b, a = signal.butter(4, normalized_cutoff_freq, btype='lowpass')
     filtered_signal = signal.lfilter(b, a, signal_data)
     return filtered_signal
 
 
-# changing the frequency of the signal
 def change_frequency(signal_data, freq_shift):
-    time = np.arrange(len(signal_data)) / SAMPLING_RATE
+    """Shifts the frequency of the signal.
+
+    Frequency shifting involves modifying the input signal by adding or subtracting a constant frequency value. This
+    function performs frequency shifting by multiplying the signal with a complex exponential waveform.
+
+    Args:
+        signal_data (numpy.ndarray): The input signal.
+        freq_shift (float): The amount of frequency shift.
+
+    Returns:
+        numpy.ndarray: The shifted signal.
+    """
+    time = np.arange(len(signal_data)) / SAMPLING_RATE
     complex_signal = signal_data * np.exp(1j * 2 * np.pi * freq_shift * time)
     shifted_signal = np.real(complex_signal)
     return shifted_signal
 
 
-# playing audio
 def play_audio(audio_data):
+    """Plays the audio.
+
+    Args:
+        audio_data (numpy.ndarray): The audio data to be played.
+    """
     sd.play(audio_data, SAMPLING_RATE)
     sd.wait()
 
 
+# Define the input file path
+input_file_path = "input.txt"
+
+# Read the input signal from the file
+input_signal = read_input_file(input_file_path)
+
+# Main program loop
+while True:
+    frequency = int(input("Enter the desired radio frequency (KHz): "))
+
+    # Apply bandpass filter to the input signal
+    filtered_signal = apply_bandpass_filter(input_signal, frequency)
+
+    # Change the frequency of the filtered signal
+    shifted_signal = change_frequency(filtered_signal, frequency)
+
+    # Play the audio
+    play_audio(shifted_signal)
+
+    choice = input("Enter 'q' to quit or any other key to continue: ")
+    if choice.lower() == "q":
+        break
